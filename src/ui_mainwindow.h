66de765f14611b01b2f4e2aa65839bdb4b7c58c8
Duncan Donaldson <donaldsonduncan@ymail.com>
2011-03-19 08:10:37 -0700

    uhh some stuff

--- src/ui_mainwindow.h
+++ src/ui_mainwindow.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: 
-**      by: Qt User Interface Compiler version 4.6.3
+** Created: Sat Mar 19 08:10:08 2011
+**      by: Qt User Interface Compiler version 4.7.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
